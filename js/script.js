5e51ec235e4d78032e7c31c568e898809ae17d17
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2021-12-03 14:14:36 +0100

    Inizio esercizio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// per prima cosa creo due variabili const,
+// la variabile priceKm è un float
+const output= document.getElementById('output');
+const priceKm = 0.21 ;
+// chiedo all'utente quanti km deve percorrere e la sua età
+
+
+// calcolo il prezzo del biglietto granzie ai km che l'utente deve percorrere
+
+
+// successivamente eseguo lo sconto in base all'età dell'utente
