87cc390368eef4dc53f1f808a8b130a2c2b604de
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2021-12-03 14:24:57 +0100

    Chiesto km da percorrere e l'età. Calcolato anche il prezzo del ticket, senza sconto

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 // per prima cosa creo due variabili const,
 // la variabile priceKm è un float
 const output= document.getElementById('output');
-const priceKm = 0.21 ;
-// chiedo all'utente quanti km deve percorrere e la sua età
+const priceKm = 0.21;
 
+// chiedo all'utente quanti km deve percorrere e la sua età
+let km = parseFloat(prompt('Quanti Km devi percorrere?'));
+km.toFixed(2)
+let age = parseInt(prompt('Quanti anni hai?'))
 
 // calcolo il prezzo del biglietto granzie ai km che l'utente deve percorrere
-
+let ticketPrice = km * priceKm;
+console.log(ticketPrice);
 
 // successivamente eseguo lo sconto in base all'età dell'utente
