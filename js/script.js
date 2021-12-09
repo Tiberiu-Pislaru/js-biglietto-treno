62695c5e01d333b5d6b8da008faedc8b56c29a4d
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2021-12-09 12:44:25 +0100

    Aggiunta Evento Click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // per prima cosa creo due variabili const,
 // la variabile priceKm è un float
 const output= document.getElementById('output');
+const result= document.getElementById('button');
 const priceKm = 0.21;
 const minorenni = 0.2;
 const over65 = 0.4;
@@ -15,36 +16,43 @@ let ticketPrice = km * priceKm;
 console.log(ticketPrice);
 let discount;
 
+result.addEventListener('click', function(){
+
+    // output.classList.remove('hide');
+
+
+    if (age < 18) {
+
+        discount = ticketPrice * minorenni;
+
+        ticketPrice = ticketPrice - discount;
+
+        // discountedTicket= discountedTicket.toFixed(2);
+
+        console.log(ticketPrice);
+
+        output.innerHTML = `Il prezzo del tuo biglietto è di: ${parseFloat(ticketPrice.toFixed(2))} €.`;
+
+    }
+
+    else if (age > 65) {
+
+        discount = ticketPrice * over65;
+
+        ticketPrice = ticketPrice - discount;
+
+        // discountedTicket = discountedTicket.toFixed(2);
+
+        console.log(ticketPrice);
+
+        output.innerHTML = `Il prezzo del tuo biglietto è di: ${parseFloat(ticketPrice.toFixed(2))} €.`;
+    }
+
+    // questo else finale rigurda la fascia d'età che viene esclusa dallo sconto
+    else {
+
+        output.innerHTML = `Il prezzo del tuo biglietto è di: ${parseFloat(ticketPrice)} €.`;
+    }
+})
+
 // successivamente eseguo lo sconto in base all'età dell'utente
-if (age < 18){
-
-    discount = ticketPrice * minorenni;
-
-    ticketPrice = ticketPrice - discount;
-
-    // discountedTicket= discountedTicket.toFixed(2);
-    
-    console.log(ticketPrice);
-    
-    output.innerHTML = `Il prezzo del tuo biglietto è di: ${ticketPrice.toFixed(2)} €.`;
-    
-}
-
-else if (age > 65){
-
-    discount = ticketPrice * over65;
-
-    ticketPrice = ticketPrice - discount;
-
-    // discountedTicket = discountedTicket.toFixed(2);
-
-    console.log(ticketPrice);
-
-    output.innerHTML = `Il prezzo del tuo biglietto è di: ${ticketPrice.toFixed(2)} €.`;
-}
-
-// questo else finale rigurda la fascia d'età che viene esclusa dallo sconto
-else{
-
-    output.innerHTML = `Il prezzo del tuo biglietto è di: ${ticketPrice} €.`;
-}
